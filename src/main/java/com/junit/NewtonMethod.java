bfb0986bd51afe26dbb278e96e7dc7cb9b9d21e1
suganthsuji <suganthpv.007@gmail.com>
2022-07-17 16:05:00 +0530

    Added: Junit program

---
+++ src/main/java/com/junit/NewtonMethod.java
@@ -0,0 +1,17 @@
+package com.junit;
+
+public class NewtonMethod {
+    /*
+  Using SqrtNewtonMethod
+   */
+    public static void main(String[] args) {
+        double c = Double.parseDouble(args[0]);
+        double t = c;
+        double epsilon = 1e-15;
+
+        while (Math.abs( t - c / t) > epsilon * t) {
+            t = ( c / t + t) / 2.0;
+        }
+        System.out.println(t);
+    }
+}
